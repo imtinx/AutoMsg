daad2f24b1344b84a9fe4e1ee43c28800f0f5151
zhijieliu <meajhlp@gmail.com>
2013-03-19 11:48:50 +0800

    增加任务配置基本界面

--- src/com/sohu/wls/app/automsg/MyActivity.java
+++ src/com/sohu/wls/app/automsg/MyActivity.java
@@ -1,7 +1,12 @@
 package com.sohu.wls.app.automsg;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.sohu.wls.app.automsg.taskconfig.TaskConfigMainActivity;
+import org.apache.http.protocol.HttpService;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +16,18 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button button = (Button) findViewById(R.id.button_task_config);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //To change body of implemented methods use File | Settings | File Templates.
+                Intent intent = new Intent();
+                intent.setClass(MyActivity.this, TaskConfigMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
